140dc3a3ff7eec95160c9850beef0cdcb823c1d6
Avany-Jocya <146840337+Avany-Jocya@users.noreply.github.com>
2024-01-16 16:11:45 +0100

    up

--- www/views/template.view.php
+++ www/views/template.view.php
@@ -2,6 +2,8 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="description" content="rien du tout">
+    <meta name="keywords" content="livres, fanfiction">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://bootswatch.com/5/sketchy/bootstrap.min.css">
     
